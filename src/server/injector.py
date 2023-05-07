bc54bcc6006cd044666cbca9d6d87d337538c83c
injae <8687lee@gmail.com>
2023-05-08 02:13:30 +0900

    update project config with pydantic BaseSettings

--- src/server/injector.py
+++ src/server/injector.py
@@ -3,16 +3,18 @@ from typing import Annotated
 from dependency_injector import containers, providers
 from dependency_injector.wiring import Provide
 from fastapi import Depends
-from server.services.redis import RedisConfig, RedisService, redis_session
+
+from server.services.redis import RedisService, redis_session
+from server.setting import Setting
 
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
         modules=[".api", ".api.v1", ".api.v1.store"],
     )
-    config = providers.Configuration(yaml_files="config.yml")
+    config = providers.Configuration(pydantic_settings=[Setting()])  # type: ignore
 
-    redis = providers.Resource(redis_session, RedisConfig())
+    redis = providers.Resource(redis_session, config.redis)
 
     redis_service = providers.Factory(RedisService, redis)
 
---
+++ src/server/setting.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+from server.services.redis import RedisConfig
+
+
+class Setting(BaseSettings):
+    redis: RedisConfig = RedisConfig()
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
